898a68a925e2dae2a4db129d3f2ef478f56c0041
Jory <jbb140030@utdallas.edu>
2021-02-09 12:24:02 -0600

    Added Win-Lose State Messages

--- BlackJory_P01A/Assets/Scripts/GameInput.cs
+++ BlackJory_P01A/Assets/Scripts/GameInput.cs
@@ -5,6 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameInput : MonoBehaviour
 {
+
+    public GameObject youWinText;
+    public GameObject youLoseText;
+
+    public static GameInput instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Backspace))
@@ -18,5 +32,15 @@ public class GameInput : MonoBehaviour
         int activeSceneIndex =
             SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(activeSceneIndex);
+    }
+
+    public void YouWin()
+    {
+        youWinText.SetActive(true);
+    }
+
+    public void YouLose()
+    {
+        youLoseText.SetActive(true);
     }
 }
--- BlackJory_P01A/Assets/Scripts/WinVolume.cs
+++ BlackJory_P01A/Assets/Scripts/WinVolume.cs
@@ -1,28 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class HazardVolume : MonoBehaviour
+public class WinVolume : MonoBehaviour
 {
-
-
     private void OnTriggerEnter(Collider other)
     {
         // detect if it's the player
         PlayerShip playerShip
             = other.gameObject.GetComponent<PlayerShip>();
-        if(playerShip != null)
+        if (playerShip != null)
         {
             //do something!
             playerShip.Kill();
-
-            GameInput.instance.YouLose();
+            GameInput.instance.YouWin();
         }
 
-
     }
-
-  
-
 }
