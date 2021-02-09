7918e90f9185832de805a692bf3cd8145b4b756d
Jory <jbb140030@utdallas.edu>
2021-02-09 10:38:05 -0600

    Add First Commit

---
+++ BlackJory_P01A/Assets/Scripts/HazardVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardVolume : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // detect if it's the player
+        PlayerShip playerShip
+            = other.gameObject.GetComponent<PlayerShip>();
+        if(playerShip != null)
+        {
+            //do something!
+            playerShip.Kill();
+        }
+
+    }
+}
